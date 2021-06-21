16446dae792193fe6e0bd20b01f8ca4918213787
Daniel Versoza <daniel.ve3@gmail.com>
2021-06-20 22:11:49 -0300

    Update ui.py
    Adjusted popup window to make it reusable

--- ui.py
+++ ui.py
@@ -31,33 +31,40 @@ class BaseUI:
 
         win.geometry(f'{self.app_width}x{self.app_height}+{int(x)}+{int(y)}')
 
+    def mainframe(self, root):
+        _frame = Frame(root)
+        _frame.grid(column=0, row=0)
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        return _frame
+
+    def popup(self, root):
+        _window = Toplevel(root)
+        _window.focus()
+        self.center_window(_window)
+        _frame = self.mainframe(_window)
+        return (_window, _frame)
+
 
 class AkiFoodUI(BaseUI):
-    def __init__(self, root, db) -> None:
+    def __init__(self, root) -> None:
         super().__init__(root)
         self.game = Game()
-        self.db = db
         self.main_interface()
 
     def main_interface(self):
-        mainframe = Frame(self.root)
-        mainframe.grid(column=0, row=0)
-        self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
+        _frame = self.mainframe(self.root)
 
-        Label(mainframe, text='Seja bem-vindo ao AkiFood!').grid()
-        Label(mainframe, text='Pense em um prato que gosta ...').grid()
-        Button(mainframe, text="OK", padx=25, pady=5,
+        Label(_frame, text='Seja bem-vindo ao AkiFood!').grid()
+        Label(_frame, text='Pense em um prato que gosta ...').grid()
+        Button(_frame, text="OK", padx=25, pady=5,
                command=self.start_game).grid()
 
-        self.root.bind("<Return>", self.start_game)
-
     def start_game(self):
-        self.game.run(db=self.db)
+        # Calls Game class and manage runtime
+        self.game.run(db=DISHES)
 
         for adjective in self.game.adjectives:
-            print(self.game.current_game_adjectives,
-                  self.game.current_game_dishes)
             if len(self.game.current_game_dishes) > 1:
                 self.attempt_adjective_form(adjective)
 
@@ -72,120 +79,96 @@ class AkiFoodUI(BaseUI):
     def attempt_adjective_form(self, adjective: str):
         def true_btn():
             self.game.update_adjective_status(adjective, True)
-            attempt_adjective_window.destroy()
+            _window.destroy()
 
         def false_btn():
             self.game.update_adjective_status(adjective, False)
-            attempt_adjective_window.destroy()
+            _window.destroy()
 
-        attempt_adjective_window = Toplevel(self.root)
-        mainframe = Frame(attempt_adjective_window)
-        mainframe.grid(column=0, row=0)
-        attempt_adjective_window.columnconfigure(0, weight=1)
-        attempt_adjective_window.rowconfigure(0, weight=1)
-        self.center_window(attempt_adjective_window)
-        Label(mainframe,
-              text=f'O prato que você pensou é {adjective}?').grid(columnspan=2)
-        yes_btn = Button(mainframe, padx=25, pady=5,
-                         text="Sim", command=true_btn)
+        _window, _frame = self.popup(self.root)
+
+        question = Label(
+            _frame, text=f'O prato que você pensou é {adjective}?')
+        question.grid(column=0, row=0, columnspan=2)
+        yes_btn = Button(_frame, padx=25, pady=5, text="Sim", command=true_btn)
         yes_btn.grid(column=0, row=1)
-        no_btn = Button(mainframe, padx=25, pady=5,
-                        text="Não", command=false_btn)
+        no_btn = Button(_frame, padx=25, pady=5, text="Não", command=false_btn)
         no_btn.grid(column=1, row=1)
-        self.root.wait_window(attempt_adjective_window)
+
+        self.root.wait_window(_window)
 
     def attempt_dish_form(self, dish: dict):
         def true_btn():
             mb.showinfo('Uhul!', 'Acertei de novo!')
             self.reload_game()
-            attempt_dish_window.destroy()
+            _window.destroy()
 
         def false_btn():
             self.new_dish_form(wrong_dish=dish)
-            attempt_dish_window.destroy()
+            _window.destroy()
 
-        attempt_dish_window = Toplevel(self.root)
-        mainframe = Frame(attempt_dish_window)
-        mainframe.grid(column=0, row=0)
-        attempt_dish_window.columnconfigure(0, weight=1)
-        attempt_dish_window.rowconfigure(0, weight=1)
-        self.center_window(attempt_dish_window)
-        Label(mainframe,
-              text=f"Ahá! Então seu prato é {dish['_name']}?").grid(columnspan=2)
-        yes_btn = Button(mainframe, text="Sim",
-                         padx=25, pady=5, command=true_btn)
+        _window, _frame = self.popup(self.root)
+
+        Label(_frame, text=f"Ahá! Então seu prato é {dish['_name']}?").grid(
+            columnspan=2)
+        yes_btn = Button(_frame, text="Sim", padx=25, pady=5, command=true_btn)
         yes_btn.grid(column=0, row=1)
-        no_btn = Button(mainframe, text="Não",
-                        padx=25, pady=5, command=false_btn)
+        yes_btn.focus()
+        no_btn = Button(_frame, text="Não", padx=25, pady=5, command=false_btn)
         no_btn.grid(column=1, row=1)
-        self.root.wait_window(attempt_dish_window)
+
+        self.root.wait_window(_window)
 
     def new_dish_form(self, wrong_dish: dict = None):
         def button_handler():
             if wrong_dish:
                 self.new_adjective_form(new_dish_name.get(), wrong_dish)
-                new_dish_window.destroy()
+                _window.destroy()
 
             else:
                 self.game.create_new_dish(new_dish_name.get())
-                new_dish_window.destroy()
+                _window.destroy()
                 self.reload_game()
 
-        new_dish_window = Toplevel(self.root)
-        mainframe = Frame(new_dish_window)
-        mainframe.grid(column=0, row=0)
-        new_dish_window.columnconfigure(0, weight=1)
-        new_dish_window.rowconfigure(0, weight=1)
-        self.center_window(new_dish_window)
-        Label(mainframe,
-              text='Aproveitando que você ainda está ai...').grid()
-        Label(mainframe, text='Em que prato pensou?').grid()
+        _window, _frame = self.popup(self.root)
+
+        self.center_window(_window)
+        Label(_frame, text='Aproveitando que você ainda está ai...').grid()
+        Label(_frame, text='Em que prato pensou?').grid()
 
         new_dish_name = StringVar()
-        new_dish_name_entry = Entry(
-            mainframe, textvariable=new_dish_name)
+        new_dish_name_entry = Entry(_frame, textvariable=new_dish_name)
         new_dish_name_entry.grid()
 
         new_dish_name_entry.focus()
 
-        Button(new_dish_window, text="OK", padx=25, pady=5,
+        Button(_window, text="OK", padx=25, pady=5,
                command=button_handler).grid()
         self.root.bind("<Return>", button_handler)
 
     def new_adjective_form(self, new_dish_name, wrong_dish):
-        def button_handler():
-            self.get_smarter_caller(
-                wrong_dish, new_dish_name, new_adjective.get())
-            new_dish_attr_window.destroy()
+        def ok_btn():
+            self.game.get_smarter(wrong_dish=wrong_dish,
+                                  new_dish=new_dish_name,
+                                  new_adjective=new_adjective.get())
+            _window.destroy()
+            self.reload_game()
 
-        new_dish_attr_window = Toplevel(self.root)
-        mainframe = Frame(new_dish_attr_window)
-        mainframe.grid(column=0, row=0)
-        new_dish_attr_window.columnconfigure(0, weight=1)
-        new_dish_attr_window.rowconfigure(0, weight=1)
-        self.center_window(new_dish_attr_window)
-        Label(mainframe,
-              text="Se puder me ajudar a ficar mais inteligente ...").grid()
-        Label(mainframe,
-              text=f"{new_dish_name} é ______, mas {wrong_dish['_name']} não.").grid()
+        _window, _frame = self.popup(self.root)
+
+        Label(_frame, text="Se puder me ajudar a ficar mais inteligente ...").grid()
+        Label(
+            _frame, text=f"{new_dish_name} é ______, mas {wrong_dish['_name']} não.").grid()
 
         new_adjective = StringVar()
-        new_adjective_entry = Entry(
-            mainframe, textvariable=new_adjective)
-        new_adjective_entry.grid()
-
-        new_adjective_entry.focus()
-
-        Button(mainframe, text="OK", padx=25, pady=5,
-               command=button_handler).grid()
-
-        self.root.bind("<Return>", button_handler)
-
-    def get_smarter_caller(self, wrong_dish, new_dish, new_adjective):
-        self.game.get_smarter(wrong_dish, new_dish, new_adjective)
-        self.reload_game()
+        _entry = Entry(_frame, textvariable=new_adjective)
+        _entry.grid()
+        _entry.focus()
+        ok_btn = Button(_frame, text="OK", padx=25, pady=5, command=ok_btn)
+        ok_btn.grid()
 
     def reload_game(self):
-        # Updates db from last run
-        self.db = self.game.dishes_db
+        # Updates db from last game run, preserving entries to next run
+        global DISHES
+        DISHES = self.game.dishes_db
         self.main_interface()
