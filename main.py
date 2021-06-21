d139e6bcb994f26bed89a09b7672437531aa1203
Daniel Versoza <daniel.ve3@gmail.com>
2021-06-20 22:11:10 -0300

    Update main.py
    Fixed dependencies and settings

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ from tkinter import *
 
 from ui import AkiFoodUI
 
-# Base dishes to start application
-dishes_db = [
-    {'_name': 'Bolo de chocolate', 'massa': False},
-    {'_name': 'Lasanha', 'massa': True}
-]
-
 if __name__ == '__main__':
     """Program entry point"""
     ui_root = Tk()
-    AkiFoodUI(ui_root, db=dishes_db)
+    AkiFoodUI(ui_root)
     ui_root.mainloop()
