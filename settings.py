3aea1268cf673f6966f24a3eb57ac9083e665b18
Daniel Versoza <daniel.ve3@gmail.com>
2021-06-20 22:11:22 -0300

    Update settings.py
    Added dishes DB to settings file

--- settings.py
+++ settings.py
@@ -3,3 +3,9 @@
 APP_TITLE = 'AkiFood'
 APP_DEFAULT_WIDTH = 300
 APP_DEFAULT_HEIGHT = 100
+
+# Base dishes to start application
+DISHES = [
+    {'_name': 'Bolo de chocolate', 'massa': False},
+    {'_name': 'Lasanha', 'massa': True}
+]
